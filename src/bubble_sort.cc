dfa7f0a4ab8f8e3a83cf8f5f0d9a3886c4ed88aa
jbehring45 <jbehring@villanova.edu>
2025-11-03 21:02:01 +0000

    Added in a file to test algorithms and some of bubble_sort

--- src/bubble_sort.cc
+++ src/bubble_sort.cc
@@ -1,5 +1,11 @@
 #include "myheaders.h"
 
 void bubble_sort(long arr[], int size) {
-
+	for(int i = 1; i < size-1; ++i){
+		for(int j = 0; j < size-2; ++j){
+			if(arr[j] > arr[j+1]){
+				swap(arr[j], arr[j+1]);
+			}
+		}
+	}
 }
