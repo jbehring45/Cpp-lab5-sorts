f204678937d89af2a366cc0f12df09ada8faa014
jbehring45 <jbehring@villanova.edu>
2025-11-05 00:03:29 +0000

    Updated Bubble sort

--- src/bubble_sort.cc
+++ src/bubble_sort.cc
@@ -1,8 +1,8 @@
 #include "myheaders.h"
 
 void bubble_sort(long arr[], int size) {
-	for(int i = 1; i < size-1; ++i){
-		for(int j = 0; j < size-2; ++j){
+	for(int i = 0; i < size-1; ++i){
+		for(int j = 0; j < size-1-i; ++j){
 			if(arr[j] > arr[j+1]){
 				swap(arr[j], arr[j+1]);
 			}
