63822071b7bc068cdb7c3eea2aea38985ce15ea8
Jackson-Lechleitner-vu <jlechlei@villanova.edu>
2025-11-04 21:05:59 -0500

    Update insertion_sort.cc

--- src/insertion_sort.cc
+++ src/insertion_sort.cc
@@ -1,22 +1,10 @@
 #include "myheaders.h"
 
-
-static void push_back(long data[], int startIdx, int endIdx) {
-	while (endIdx != startIdx) {
-		data[endIdx] = data[endIdx-1];
-		endIdx -= 1;
-	}
-}
-
-void insertion_sort(long data[], int size) {
-	long temp = 0;
-
-	for (int i=0; i<size; i++) {
-		for (int j=0; j<i; j++) {
-			if (data[i] < data[j]) {
-				temp = data[i];
-				push_back(data, j, i);
-				data[j] = temp;
+void bubble_sort(long arr[], int size) {
+	for(int i = 1; i < size-1; ++i){
+		for(int j = 0; j < size-2; ++j){
+			if(arr[j] > arr[j+1]){
+				swap(arr[j], arr[j+1]);
 			}
 		}
 	}
