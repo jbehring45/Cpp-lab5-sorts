852632a042a2ccebf9e3100942a2d514f713e39a
jbehring45 <jbehring@villanova.edu>
2025-11-04 19:08:28 -0500

    Created test file

---
+++ src/test.cc
@@ -0,0 +1,15 @@
+//File to test out sorting algorithms on a smaller scale
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include "myheaders.h"
+
+int main(){
+	int size = 1;
+	long arr[size] = {1};
+	bubble_sort(arr, size);
+	printarray(arr, size);
+	return 1;
+}
