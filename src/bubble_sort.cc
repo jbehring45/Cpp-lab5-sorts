7de8f1fff6cea10d13d90d4272382bc04bce5b40
jbehring45 <jbehring@villanova.edu>
2025-11-09 19:00:44 -0500

    Update bubble_sort.cc
    Add Comments to Bubble_sort

--- src/bubble_sort.cc
+++ src/bubble_sort.cc
@@ -1,7 +1,10 @@
 #include "myheaders.h"
 
+//Function to bubble sort an array
 void bubble_sort(long arr[], int size) {
+	//Loop to run through the array the needed times to make sure it is fully sorted.
 	for(int i = 0; i < size-1; ++i){
+		//Loop to run through the array and compare elements and swap as nedded
 		for(int j = 0; j < size-1-i; ++j){
 			if(arr[j] > arr[j+1]){
 				swap(arr[j], arr[j+1]);
